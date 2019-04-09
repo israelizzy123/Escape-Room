2fef0c92ff63a932ac8f4b4e47f74c34879dc3a5
israelizzy123 <31420706+israelizzy123@users.noreply.github.com>
2019-04-09 14:18:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,6 @@ g = document.getElementById("7");
 h = document.getElementById("8");
 i = document.getElementById("9");
 // the document.getElementById's actually put them into html and also to make to put it into the function checkImage to make them a,b,c,d,e,f,g,h,i and the functions rotateImage is setting up by using the onclick to do the classlist.toggle and also to add class and also in the if else if *//
-var img = document.getElementById('blinking_image');
-
-var interval = window.setInterval(function(){
-    if(img.display == 'hidden'){
-        img.style.visibility = 'visible';
-    }else{
-        img.style.visibility = 'hidden';
-    }
-}, 1000);
 
 function rotateImage(el){
 	el.classList.toggle("rotate");
@@ -49,20 +40,5 @@ function checkImage(el){
 	}
 
 }
-function on(){
-	if (document.getElementById)
-		//the value for visibility of the logo image with will visible
-	document.getElementById("blink1").style.visibility = "visible";
-}
-//this function is to hide the logo with the value hidden
-function off(){
-	if (document.getElementById)
-	document.getElementById("blink1").style.visibility = "hidden";
-}
 
-// toggle "on" and "off"every 500 ms to show the nhl logo blinking
-// end after 5500 ms which is about 5 seconds
-for(var i=900; i < 5500; i=i+900){
-	setTimeout("off()",i);
-	setTimeout("on()",i+500);
-}
+
